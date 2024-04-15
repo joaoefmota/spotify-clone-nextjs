734e8f4554fbe97ecfd2ca5bf0214cabc57105a6
João Mota <joaoefmota@gmail.com>
2024-04-15 19:53:18 +0200

    Header and Button intial implementation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,11 @@
 "use client";
 import { useRouter } from "next/navigation";
 import React, { FunctionComponent, PropsWithChildren } from "react";
+import { BiSearch } from "react-icons/bi";
 import { HiHome } from "react-icons/hi";
 import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
 import { twMerge } from "tailwind-merge";
+import Button from "./Button";
 
 type HeaderProps = PropsWithChildren<{}> & {
   className?: string;
@@ -41,13 +43,41 @@ export const Header: FunctionComponent<HeaderProps> = ({
             <RxCaretRight size={35} className="text-white" />
           </button>
         </div>
-        <div className="flex md_hidden gap-x-2 items-center">
-          <button type="button" title="home-button">
+        <div className="flex md:hidden gap-x-2 items-center">
+          <button
+            type="button"
+            className="rounded-full p-2 bg-white flex items-center justify-center hover:opacity-75 transition"
+            title="home-button"
+          >
             <HiHome size={20} className="text-black" />
           </button>
-          <button type="button" title=""></button>
+          <button
+            type="button"
+            className="rounded-full p-2 bg-white flex items-center justify-center hover:opacity-75 transition"
+            title="home-button"
+          >
+            <BiSearch size={20} className="text-black" />
+          </button>
+        </div>
+        <div className="flex justify-between items-center gap-x-4">
+          <>
+            <div>
+              <Button
+                className="bg-transparent text-neutral-300 font-medium"
+                onClick={() => {}}
+              >
+                Sign up
+              </Button>
+            </div>
+            <div>
+              <Button className="bg-white px-6 py-2" onClick={() => {}}>
+                Log In
+              </Button>
+            </div>
+          </>
         </div>
       </div>
+      {children}
     </div>
   );
 };
